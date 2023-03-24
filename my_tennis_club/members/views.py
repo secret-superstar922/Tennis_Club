fb77ed4fb6d1993fb68cde4b43d9995afd3562ae
hit-dev <leonpwlwsk0713@gmail.com>
2023-03-25 08:18:43 +0900

    setting url

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def members(request):
-    return HttpResponse("Hello World") 
+    # template = loader.get_template('myfirst.html')
+    return HttpResponse("Hello World")
